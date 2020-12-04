c4762f1680a20691ca26e4724a5f1a996a805fa2
RaulC2803 <raul.casanova.03@gmail.com>
2020-12-04 00:04:56 -0500

    work done

--- video_summarization.py
+++ video_summarization.py
@@ -11,7 +11,7 @@ def getContours(img):
 
 
 # get the binary from the difference between frame and base images
-def recognizePerson(original, img, bg, result,  lista, timeini, minutes):
+def recognizePerson(original, img, bg, lista, timeini):
     kernel = np.ones((4,4),np.uint8)
     res = cv2.absdiff(img, bg)
     ret, th = cv2.threshold(res, 15, 255, cv2.THRESH_BINARY )
@@ -22,39 +22,60 @@ def recognizePerson(original, img, bg, result,  lista, timeini, minutes):
  
     cont = getContours(th)
 
-    ##cv2.imshow('difference', th)
-
     for i in cont:
         area = cv2.contourArea(i)
         if area > 4000:
             x, y, w, h = cv2.boundingRect(i)
-            summarize(original, x, y, w, h, result, lista, timeini, minutes)
+            summarize(original, x, y, w, h, lista, timeini)
             break
       
 
-   
-
 # get only the parts where the area's contours is > 5000
 # that parts are save in the result video
-def summarize(img, x, y, w, h, result, lista,timeini, minutes):
-    seconds = ti.time() - timeini
-    print(round(seconds)% 60 == 0.0)
-    if round(seconds) % 60 == 0.0:
-        minutes += 1
-    timetext = str(minutes) + ':' + str(seconds)
+def summarize(img, x, y, w, h, lista, timeini):
+    seconds = round(ti.time() - timeini) % 60
+    minute = int((ti.time() - timeini) // 60)
+
+    if seconds > 9:
+        seconds = str(seconds)
+    else:
+        seconds = '0' + str(seconds)
+    if minute > 9:
+        timetext = str(minute) + ':' + seconds
+    else:
+        timetext = '0' + str(minute) + ':' + seconds
+
     cv2.putText(img, timetext , (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
-    result.write(img)
-    lista.append((img, ti.time() - timeini))
-    print(ti.time()- timeini)
+    lista.append([img, round(ti.time() - timeini, 4)])
     cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
 
 
 
-##def summarizepart2(lista):
-##    for i in range(0,len(lista)):
-##        if lista[i+1][1] - lista[i][1] >= 3:
-##            print()
+def summarizepart2(lista):
+    index = []
+    j = 0
+    for i in range(len(lista) - 1):
+        diff = round(lista[i+1][1] - lista[i][1], 4)
+        if diff >= 1:
+            div = (i - j) // 2 + j
+            j = i
+            index.append([div, i])
 
+    return index
+
+
+def writeVideo(result, lista, index):
+    delete = []
+    for i in range(len(index)):
+        first = index[i][0]
+        second = index[i][1]
+        for j in range(second - first):
+            lista[first + j][0] = cv2.addWeighted(lista[first + j][0], 0.5, lista[second + j][0], 0.5, 0)
+            delete.append(second + j)
+    for i in range(len(lista)):
+        if not i in delete:
+            result.write(lista[i][0])
+    
 
 
 #Play the result video
@@ -67,16 +88,12 @@ def playVideo():
         cv2.imshow('Video result', frame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
-    cat.release()
+    cat.release() 
 
-##def summarizepart2(lista, result):
-##    for i in range(0,len(lista)):
-        
 
 if __name__=='__main__':
     lista = []
     timeini = ti.time()
-    minutes = 0
     # get the video
     cat = cv2.VideoCapture('./resources/test3.mp4')
 
@@ -88,7 +105,6 @@ if __name__=='__main__':
     # convert to gray 2
     back_gray = cv2.cvtColor(back, cv2.COLOR_BGR2GRAY)
 
-    cron = 0
     while True:
         #get every frame
         _, frame = cat.read()
@@ -99,19 +115,19 @@ if __name__=='__main__':
         # convert to gray the frame
         fr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        recognizePerson(frame, fr, back_gray, result,  lista,timeini, minutes)
+        recognizePerson(frame, fr, back_gray, lista, timeini)
 
-        ##cv2.imshow('videocamera', frame)
-        
-       
+        cv2.imshow('videocamera', frame)
+         
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
-    suma = cv2.addWeighted(lista[10][0],0.5,lista[len(lista)-1][0],0.5,0)
-    print(lista[0][1])
-    cv2.imshow("suma",suma)
-    cat.release()
-    result.release()
-    cv2.imshow("suma",suma)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
+    cat.release()
+
+    index = summarizepart2(lista)
+
+    writeVideo(result, lista, index)
+
+    result.release()
+
+    playVideo()
